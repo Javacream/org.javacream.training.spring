ef9b36646fc72d9db8fef81929a6c4c8cb4e33f3
Rainer Sawitzki <training@rainer-sawitzki.de>
2018-10-09 09:53:03 +0200

    add profiles

--- projects/core/boot/src/main/java/org/javacream/BooksWarehouseApplication.java
+++ projects/core/boot/src/main/java/org/javacream/BooksWarehouseApplication.java
@@ -1,12 +1,30 @@
 package org.javacream;
 
+import javax.annotation.PostConstruct;
+
+import org.javacream.books.warehouse.api.BookException;
+import org.javacream.books.warehouse.api.BooksService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BooksWarehouseApplication {
 
+	@Autowired private BooksService booksService;
 	public static void main(String[] args) {
-		SpringApplication.run(BooksWarehouseApplication.class, args);
+		SpringApplication springApplication = new SpringApplication(BooksWarehouseApplication.class);
+		springApplication.setAdditionalProfiles("prod");
+		springApplication.run(args);
+	}
+	
+	@PostConstruct public void init() {
+		try {
+			System.out.println(booksService.findAllBooks());
+			System.out.println(booksService.newBook("Spring Boot"));
+			System.out.println(booksService.findAllBooks());
+		} catch (BookException e) {
+			e.printStackTrace();
+		}
 	}
 }
