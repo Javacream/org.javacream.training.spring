7848f7e090ae23c72cfc7b32644021aed98099cc
Rainer Sawitzki <training@rainer-sawitzki.de>
2018-10-09 13:33:59 +0200

    add aspectj

---
+++ projects/core/boot/src/main/java/org/javacream/util/aspects/aspectj/TracingAspects.java
@@ -0,0 +1,30 @@
+package org.javacream.util.aspects.aspectj;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class TracingAspects {// Strange: AspectJ-Aspect class name may not end with 'Aspect'
+
+	@Around("execution(* org.javacream..*Service.*(..))")
+	public Object trace(ProceedingJoinPoint pjp) throws Throwable {
+
+		MethodSignature methodSignature = (MethodSignature) pjp.getSignature();
+
+		String name = methodSignature.getMethod().getName();
+		System.out.println("ASPECTJ: entering " + name);
+		try {
+			Object result = pjp.proceed();
+			System.out.println("ASPECTJ: returning from " + name);
+			return result;
+		} catch (Throwable e) {
+			System.out.println("ASPECTJ: throwing from " + name);
+			throw e;
+		}
+	}
+
+}
