7c6de4e4241279af528c26d4e9764613d3b70145
Rainer Sawitzki <training@rainer-sawitzki.de>
2020-01-07 16:08:41 +0100

    add aspect

---
+++ org.javacream.training.spring.books/src/main/java/org/javacream/util/aspect/TracingAspect.java
@@ -0,0 +1,28 @@
+package org.javacream.util.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class TracingAspect {
+
+	@Around("execution(* org.javacream..impl.*.*(..))")
+	public Object trace(ProceedingJoinPoint proceedingJoinPoint) throws Throwable{
+		MethodSignature methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
+		String methodName = methodSignature.getMethod().getName();
+		try {
+			System.out.println("entering method " + methodName);
+			Object result =  proceedingJoinPoint.proceed();
+			System.out.println("returning from method " + methodName);
+			return result;
+		}catch(Throwable t) {
+			System.out.println("throwing from method " + methodName);
+			throw t;
+		}
+		
+	}
+}
