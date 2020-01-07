c617b32e44aff5417fc6c8d2e9349b7e332b07cb
Rainer Sawitzki <training@rainer-sawitzki.de>
2020-01-07 14:24:59 +0100

    implement BooksWebService

--- org.javacream.training.spring.books/src/main/java/org/javacream/books/warehouse/api/Book.java
+++ org.javacream.training.spring.books/src/main/java/org/javacream/books/warehouse/api/Book.java
@@ -2,6 +2,12 @@ package org.javacream.books.warehouse.api;
 
 import java.io.Serializable;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Book implements Serializable {
 	private static final long serialVersionUID = 1L;
 
