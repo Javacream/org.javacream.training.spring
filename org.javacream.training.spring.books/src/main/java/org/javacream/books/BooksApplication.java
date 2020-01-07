534910ae50529d455eef39e66a3a8523b7801c7a
Rainer Sawitzki <training@rainer-sawitzki.de>
2020-01-07 07:01:55 +0100

    create project directory

---
+++ org.javacream.training.spring.books/src/main/java/org/javacream/books/BooksApplication.java
@@ -0,0 +1,29 @@
+package org.javacream.books;
+
+import javax.annotation.PostConstruct;
+
+import org.javacream.books.warehouse.api.Book;
+import org.javacream.books.warehouse.api.BookException;
+import org.javacream.books.warehouse.impl.MapBooksService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+//@Component
+public class BooksApplication {
+
+	@Autowired private MapBooksService mapBooksService;
+	@PostConstruct
+	public void doBooksApplication() {
+		System.out.println("Starting Books-App");
+		try {
+			String isbn = mapBooksService.newBook("Spring");
+			System.out.println("Generated isbn: " + isbn);
+			Book book = mapBooksService.findBookByIsbn(isbn);
+			System.out.println("Found book: " + book);
+		} catch (BookException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("Books-App finished");
+		
+	}
+}
