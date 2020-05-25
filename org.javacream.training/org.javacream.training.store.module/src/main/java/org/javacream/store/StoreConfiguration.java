880e1b73277409317f1ec6a1706f1a46a6aa327c
Rainer Sawitzki <training@rainer-sawitzki.de>
2020-05-25 13:40:31 +0200

    implement store module

---
+++ org.javacream.training/org.javacream.training.store.module/src/main/java/org/javacream/store/StoreConfiguration.java
@@ -0,0 +1,8 @@
+package org.javacream.store;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreConfiguration {
+
+}
---
+++ org.javacream.training/org.javacream.training.store.module/src/test/java/org/javacream/store/StoreServiceTest.java
@@ -0,0 +1,25 @@
+package org.javacream.store;
+
+import org.javacream.store.api.StoreService;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+//@ActiveProfiles("test")
+public class StoreServiceTest {
+
+	@Autowired private StoreService storeService;
+	
+	@Test public void contextLoads() {
+		Assert.assertNotNull(storeService);
+	}
+	@Test public void configurationOk() {
+		Assert.assertEquals(42, storeService.getStock("cat", "item"));
+	}
+}
