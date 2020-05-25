79b6df7a0ef48266750231b9db9bd7f95db2cf90
Rainer Sawitzki <training@rainer-sawitzki.de>
2020-05-25 14:02:29 +0200

    implement BooksApplication

--- org.javacream.training/org.javacream.training.books.application/src/main/java/org/javacream/BooksApplication.java
+++ org.javacream.training/org.javacream.training.books.application/src/main/java/org/javacream/BooksApplication.java
@@ -1,14 +1,31 @@
 package org.javacream;
 
+import javax.annotation.PostConstruct;
+
+import org.javacream.books.warehouse.api.Book;
+import org.javacream.books.warehouse.api.BookException;
+import org.javacream.books.warehouse.api.BooksService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BooksApplication {
 
+	@Autowired private BooksService booksService;
 	public static void main(String[] args) {
 		SpringApplication.run(BooksApplication.class, args);
 		System.out.println("Starting BooksApplication");
+	}
+	
+	@PostConstruct public void init() {
+		try {
+			String isbn = booksService.newBook("Spring");
+			Book book = booksService.findBookByIsbn(isbn);
+			System.out.println(book);
+		} catch (BookException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
