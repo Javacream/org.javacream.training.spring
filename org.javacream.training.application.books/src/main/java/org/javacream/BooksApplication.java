cf6ae8edd128efbbd0310541eeeba13b20713ad0
Rainer Sawitzki <training@rainer-sawitzki.de>
2020-05-25 11:33:27 +0200

    refactor to Spring Boot executable Project

---
+++ org.javacream.training.application.books/src/main/java/org/javacream/BooksApplication.java
@@ -0,0 +1,14 @@
+package org.javacream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksApplication.class, args);
+		System.out.println("Starting BooksApplication");
+	}
+
+}
