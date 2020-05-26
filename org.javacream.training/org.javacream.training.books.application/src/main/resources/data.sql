e53151f6f9185e63166d7a75d51ff547ba2dc881
Rainer Sawitzki <training@rainer-sawitzki.de>
2020-05-26 12:14:14 +0200

    finish DatabaseStoreService

---
+++ org.javacream.training/org.javacream.training.books.application/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into STORE (category, item , stock) values ('books', 'ISBN1', 42)
+insert into STORE (category, item , stock) values ('books', 'ISBN2', 4)
+insert into STORE (category, item , stock) values ('books', 'ISBN3', 2)
+insert into STORE (category, item , stock) values ('books', 'ISBN4', 24)
+insert into STORE (category, item , stock) values ('books', 'ISBN5', 44)
--- org.javacream.training/org.javacream.training.books.application/src/main/resources/schema.sql
+++ org.javacream.training/org.javacream.training.books.application/src/main/resources/schema.sql
@@ -1,2 +1,1 @@
-drop table STORE if exists
 create table STORE (category varchar(256), item varchar(256), stock integer, primary key (category, item))
