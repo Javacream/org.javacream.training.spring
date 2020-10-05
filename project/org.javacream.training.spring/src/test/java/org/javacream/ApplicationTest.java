4f5bd3748f71e42a039adce4261389ed230d6cfa
Rainer Sawitzki <training@rainer-sawitzki.de>
2020-10-05 12:47:39 +0200

    add books application

---
+++ project/org.javacream.training.spring/src/test/java/org/javacream/ApplicationTest.java
@@ -0,0 +1,10 @@
+package org.javacream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTest {
+
+	@Test void contextLoads() {}
+}
