685123a3f009911237faad07b43cb1878853039b
Rainer Sawitzki <training@rainer-sawitzki.de>
2020-11-13 15:13:02 +0100

    introduce EchoWebService

--- src/main/java/org/javacream/Application.java
+++ src/main/java/org/javacream/Application.java
@@ -1,24 +1,15 @@
 package org.javacream;
 
-import javax.annotation.PostConstruct;
-
-import org.javacream.store.api.StoreService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
 
-	private @Autowired StoreService storeService;
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 		
 	}
 	
-	public @PostConstruct void run() {
-		System.out.println("Application started");
-		System.out.println(storeService.getStock("category", "item"));
-	}
 
 }
