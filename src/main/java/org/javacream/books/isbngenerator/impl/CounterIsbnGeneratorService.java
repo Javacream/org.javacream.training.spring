c27a3e4702789f706f4c60ab2a89a30865cf59d6
Rainer Sawitzki <training@rainer-sawitzki.de>
2020-11-13 15:20:49 +0100

    implements StoreWebService, IsbnGeneratorWebService

--- src/main/java/org/javacream/books/isbngenerator/impl/CounterIsbnGeneratorService.java
+++ src/main/java/org/javacream/books/isbngenerator/impl/CounterIsbnGeneratorService.java
@@ -2,14 +2,17 @@ package org.javacream.books.isbngenerator.impl;
 
 import org.javacream.books.isbngenerator.api.IsbnGeneratorService;
 import org.javacream.books.isbngenerator.api.IsbnGeneratorService.SequenceStrategy;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
-@SequenceStrategy 
+@SequenceStrategy
 public class CounterIsbnGeneratorService implements IsbnGeneratorService {
-
+	@Value("${isbngenerator.prefix}")
 	private String prefix;
+	@Value("${isbngenerator.countryCode}")
 	private String countryCode;
+
 	public String getCountryCode() {
 		return countryCode;
 	}
@@ -17,14 +20,17 @@ public class CounterIsbnGeneratorService implements IsbnGeneratorService {
 	public void setCountryCode(String suffix) {
 		this.countryCode = suffix;
 	}
+
 	private int counter;
-	public String next(){
+
+	public String next() {
 		return prefix + counter++ + countryCode;
 	}
 
-	public String getPrefix(){
+	public String getPrefix() {
 		return prefix;
 	}
+
 	public void setPrefix(String prefix) {
 		this.prefix = prefix;
 	}
