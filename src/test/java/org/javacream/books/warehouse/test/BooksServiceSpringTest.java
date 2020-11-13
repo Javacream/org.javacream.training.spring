5bbe3de0563e0145deb2a86baef967d53d146f7a
Rainer Sawitzki <training@rainer-sawitzki.de>
2020-11-13 10:38:46 +0100

    add test configuration for BooksServiceTest

--- src/test/java/org/javacream/books/warehouse/test/BooksServiceSpringTest.java
+++ src/test/java/org/javacream/books/warehouse/test/BooksServiceSpringTest.java
@@ -3,7 +3,9 @@ package org.javacream.books.warehouse.test;
 import org.javacream.books.warehouse.api.BooksService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootTest
 public class BooksServiceSpringTest {
@@ -12,5 +14,11 @@ public class BooksServiceSpringTest {
 	
 	@Test public void testBooksService() {
 		TestActor.doTest(booksService);
+	}
+	
+	@SpringBootConfiguration 
+	@ComponentScan(basePackages= {"org.javacream.books", "org.javacream.store"})
+	public static class BooksServiceTestConfiguration{
+		
 	}
 }
