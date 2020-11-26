c6650f46fc6d381a1445f4bc38604e273ce3566c
Rainer Sawitzki <training@rainer-sawitzki.de>
2020-11-26 15:35:14 +0100

    add content webservice and couchbase example

---
+++ projects/org.javacream.training.spring.content/src/main/java/org/javacream/content/ContentApplication.java
@@ -0,0 +1,50 @@
+package org.javacream.content;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.javacream.content.api.Content;
+import org.javacream.content.api.ContentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.couchbase.core.CouchbaseOperations;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.couchbase.client.java.query.N1qlQuery;
+
+@SpringBootApplication
+@RestController
+public class ContentApplication {
+
+		@Autowired ContentService contentService;
+		@Autowired
+		private CouchbaseOperations couchbaseOperations;
+
+		@PostConstruct
+		private void postConstruct() {
+
+			// add _class attribute
+			List<Content> contentWithoutClassAttribute = couchbaseOperations.findByN1QL(N1qlQuery.simple( //
+					"SELECT META(`content`).id AS _ID, META(`content`).cas AS _CAS, `content`.* " + //
+							"FROM `content` " + //
+							"WHERE _class IS MISSING;"),
+					Content.class);
+
+			contentWithoutClassAttribute.forEach(couchbaseOperations::save);
+		}
+		
+		public static void main(String[] args) {
+			SpringApplication.run(ContentApplication.class, args);
+		}
+		
+		@GetMapping (path = "api/content/{id}", produces = MediaType.APPLICATION_JSON_VALUE) public Content findById(@PathVariable("id") String id) {
+			return contentService.findById(id); 
+		}
+	}
+	
+
---
+++ projects/org.javacream.training.spring/src/main/java/org/javacream/books/isbngenerator/api/IsbnGeneratorService.java
@@ -0,0 +1,28 @@
+package org.javacream.books.isbngenerator.api;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+
+public interface IsbnGeneratorService {
+
+	public abstract String next();
+
+	
+	@Target({ElementType.FIELD, ElementType.TYPE, ElementType.METHOD, ElementType.PARAMETER})
+	@Retention(RetentionPolicy.RUNTIME)
+	@Qualifier
+	public @interface RandomStrategy {
+
+	}
+	@Target({ElementType.FIELD, ElementType.TYPE, ElementType.METHOD, ElementType.PARAMETER})
+	@Retention(RetentionPolicy.RUNTIME)
+	@Qualifier
+	public @interface SequenceStrategy {
+
+	}
+
+}
