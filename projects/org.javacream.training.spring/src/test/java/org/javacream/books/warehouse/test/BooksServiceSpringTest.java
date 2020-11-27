74f04f0f4f88fe4dd929340278019289a277d180
Rainer Sawitzki <training@rainer-sawitzki.de>
2020-11-27 10:43:46 +0100

    fix Test

--- projects/org.javacream.training.spring/src/test/java/org/javacream/books/warehouse/test/BooksServiceSpringTest.java
+++ projects/org.javacream.training.spring/src/test/java/org/javacream/books/warehouse/test/BooksServiceSpringTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
@@ -19,7 +20,8 @@ public class BooksServiceSpringTest {
 		TestActor.doTest(booksService);
 	}
 	
-	@SpringBootApplication
+	@SpringBootApplication()
+	@EnableJpaRepositories(basePackages = "org.javacream.books")
 	@ComponentScan(basePackages= {"org.javacream.books", "org.javacream.store", "org.javacream.util.aspects"})
 	@EntityScan(basePackages = "org.javacream")
 	public static class BooksServiceTestConfiguration{
