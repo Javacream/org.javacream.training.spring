872d26dc40eac932395c84c097c2fd42a45b5965
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-01-25 17:23:29 +0100

    update spring version

--- src/test/java/org/javacream/books/warehouse/test/BooksServiceTest.java
+++ src/test/java/org/javacream/books/warehouse/test/BooksServiceTest.java
@@ -3,7 +3,7 @@ package org.javacream.books.warehouse.test;
 import org.javacream.books.isbngenerator.impl.RandomIsbnGenerator;
 import org.javacream.books.warehouse.impl.MapBooksService;
 import org.javacream.store.impl.SimpleStoreService;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 
 public class BooksServiceTest {
--- src/test/java/org/javacream/books/warehouse/test/TestActor.java
+++ src/test/java/org/javacream/books/warehouse/test/TestActor.java
@@ -1,14 +1,10 @@
 package org.javacream.books.warehouse.test;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
-
 import java.util.Collection;
-
 import org.javacream.books.warehouse.api.BookException;
 import org.javacream.books.warehouse.api.Book;
 import org.javacream.books.warehouse.api.BooksService;
+import org.junit.jupiter.api.Assertions;
 
 public class TestActor {
 
@@ -20,28 +16,28 @@ public class TestActor {
 			String isbn = booksService.newBook(j2eeTitle);
 			books = booksService.findAllBooks();
 			final int endSize = books.size();
-			assertTrue(endSize == startSize + 1);
-			assertNotNull(isbn);
-			assertNotNull(booksService.findBookByIsbn(isbn));
+			Assertions.assertTrue(endSize == startSize + 1);
+			Assertions.assertNotNull(isbn);
+			Assertions.assertNotNull(booksService.findBookByIsbn(isbn));
 
 			try {
 				booksService.findBookByIsbn("ISBN-3");
-				fail("BookException must be thrown!");
+				Assertions.fail("BookException must be thrown!");
 			} catch (BookException e) {
 				// OK
 			}
 			booksService.deleteBookByIsbn(isbn);
-			assertTrue(startSize == booksService.findAllBooks().size());
+			Assertions.assertTrue(startSize == booksService.findAllBooks().size());
 			try {
 				booksService.deleteBookByIsbn(isbn);
-				fail("BookException must be thrown!");
+				Assertions.fail("BookException must be thrown!");
 			} catch (BookException e) {
 				// OK
 			}
 
 		} catch (BookException bookException) {
 			bookException.printStackTrace();
-			fail(bookException.getMessage());
+			Assertions.fail(bookException.getMessage());
 		}
 
 	}
