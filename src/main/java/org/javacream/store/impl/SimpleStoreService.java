d6c29e1012854e11d4b6e22a2f08cb12f332aac0
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-01-26 13:41:52 +0100

    add configuration

--- src/main/java/org/javacream/store/impl/SimpleStoreService.java
+++ src/main/java/org/javacream/store/impl/SimpleStoreService.java
@@ -1,11 +1,12 @@
 package org.javacream.store.impl;
 
 import org.javacream.store.api.StoreService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SimpleStoreService implements StoreService {
-	private int stock;
+	@Value("${storeService.defaultStock}") private int stock;
 	
 	@Override
 	public int getStock(String category, String item) {
