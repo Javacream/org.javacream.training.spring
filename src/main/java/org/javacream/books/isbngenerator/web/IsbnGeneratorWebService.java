ca9002712143819d86a14197f563bd3fafb05783
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-01-28 14:26:04 +0100

    implement IsbnGeneratorWebService with fetchSize

--- src/main/java/org/javacream/books/isbngenerator/web/IsbnGeneratorWebService.java
+++ src/main/java/org/javacream/books/isbngenerator/web/IsbnGeneratorWebService.java
@@ -6,20 +6,32 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class IsbnGeneratorWebService {
 
 @Autowired @IsbnGeneratorService.RandomStrategy private IsbnGeneratorService randomIsbnGenerator;
 @Autowired @IsbnGeneratorService.SequenceStrategy private IsbnGeneratorService sequenceIsbnGenerator;
 
-//curl -H "strategy: random" -X POST http://localhost:9090/api/isbn
+//curl -H "strategy: random" -H "fetchSize: 10" -X POST http://localhost:9090/api/isbn
 
-@PostMapping(path = "api/isbn") public String isbn(@RequestHeader("strategy") String strategy){
+@PostMapping(path = "api/isbn", produces = "application/json") public List<String> isbn(@RequestHeader("strategy") String strategy, @RequestHeader("fetchSize") int fetchSize){
     if("sequence".equals(strategy)){
-        return sequenceIsbnGenerator.next();
+        return assemble(sequenceIsbnGenerator, fetchSize);
     }else{
-        return randomIsbnGenerator.next();
+        return assemble(randomIsbnGenerator, fetchSize);
     }
+}
+
+private List<String> assemble(IsbnGeneratorService isbnGenerator, int fetchSize){
+    ArrayList<String> result = new ArrayList<>(fetchSize);
+    for (int i = 0; i < fetchSize; i++){
+        result.add(isbnGenerator.next());
+    }
+    return result;
 }
 
 
