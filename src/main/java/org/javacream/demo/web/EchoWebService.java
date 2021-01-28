11d447fd940f277690edbf9632b28ac258d2e411
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-01-28 11:19:18 +0100

    introduce StoreWebService

--- src/main/java/org/javacream/demo/web/EchoWebService.java
+++ src/main/java/org/javacream/demo/web/EchoWebService.java
@@ -1,8 +1,7 @@
 package org.javacream.demo.web;
 
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class EchoWebService {
@@ -11,4 +10,28 @@ public class EchoWebService {
     public String ping(){
         return "pong";
     }
+
+    //http://localhost:8080/echo/Hello
+    @GetMapping(path = "echo/{m}", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String echoWithPathVariable(@PathVariable("m") String message){
+        System.out.println("Echoing " + message);
+        return new StringBuilder(message).reverse().toString();
+    }
+
+    //http://localhost:8080/echo?mess=Hello
+    @GetMapping(path = "echo", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String echoWithQueryParam(@RequestParam("mess") String message){
+        System.out.println("Echoing " + message);
+        return new StringBuilder(message).reverse().toString();
+    }
+
+
+
+    //curl -X GET -H "mess: Hello" http://localhost:8080/echoWithHeader
+    @GetMapping(path = "echoWithHeader", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String echoWithHeaderParam(@RequestHeader("mess") String message){
+        System.out.println("Echoing " + message);
+        return new StringBuilder(message).reverse().toString();
+    }
+
 }
