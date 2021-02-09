1f995243d0532e3b70df46a91be14822f44b4e3c
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2021-02-09 20:16:06 +0100

    initialize project using Spring Boot and Spring MVC/Rest

---
+++ src/main/java/org/javacream/ApplicationConfiguration.java
@@ -0,0 +1,22 @@
+package org.javacream;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.javacream.books.warehouse.api.Book;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("prod")
+public class ApplicationConfiguration {
+
+
+	@Bean
+	public Map<String, Book> books() {
+		return new HashMap<>();
+	}
+
+	
+}
---
+++ src/main/java/org/javacream/books/warehouse/web/BooksWebService.java
@@ -0,0 +1,66 @@
+package org.javacream.books.warehouse.web;
+
+import java.util.Collection;
+
+import org.javacream.books.warehouse.api.Book;
+import org.javacream.books.warehouse.api.BookException;
+import org.javacream.books.warehouse.api.BooksService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+public class BooksWebService {
+
+	@Autowired
+	private BooksService booksService;
+
+	@PostMapping(path = "api/books/{title}", produces = MediaType.TEXT_PLAIN_VALUE)
+	public String newBook(@PathVariable("title") String title) {
+		try {
+			return booksService.newBook(title);
+		} catch (BookException e) {
+			throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, e.getMessage());
+		}
+	}
+
+	@GetMapping(path = "api/books/{isbn}", produces = MediaType.APPLICATION_JSON_VALUE )
+	public Book findBookByIsbn(@PathVariable("isbn") String isbn) {
+		try {
+			return booksService.findBookByIsbn(isbn);
+		} catch (BookException e) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+		}
+	}
+
+	@PutMapping(path = "api/books/{isbn}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public Book updateBook(@PathVariable("isbn") String isbn, @RequestBody Book book) {
+		try {
+			return booksService.updateBook(book);
+		} catch (BookException e) {
+			throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, e.getMessage());
+		}
+	}
+
+	@DeleteMapping(path = "api/books/{isbn}")
+	public void deleteBookByIsbn(@PathVariable("isbn")  String isbn) {
+		try {
+			booksService.deleteBookByIsbn(isbn);
+		} catch (BookException e) {
+			throw new ResponseStatusException(HttpStatus.CONFLICT, e.getMessage());
+		}
+	}
+
+	@GetMapping(path = "api/books", produces = MediaType.APPLICATION_JSON_VALUE)
+	public Collection<Book> findAllBooks() {
+		return booksService.findAllBooks();
+	}
+}
