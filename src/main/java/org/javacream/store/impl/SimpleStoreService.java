fe492b28410c4923041dc13f5e50bc4851773337
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-02-11 14:17:51 +0100

    finish simple application.properties

--- src/main/java/org/javacream/store/impl/SimpleStoreService.java
+++ src/main/java/org/javacream/store/impl/SimpleStoreService.java
@@ -1,10 +1,12 @@
 package org.javacream.store.impl;
 
 import org.javacream.store.api.StoreService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SimpleStoreService implements StoreService {
+	@Value("${defaultStock}")
 	private int stock;
 	
 	@Override
