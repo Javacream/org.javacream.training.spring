8934d3fb1968af31687d07be35bde4aa3a60b3f5
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-03-10 12:02:21 +0100

    refactoring project

---
+++ projects/org.javacream.books.library/src/main/java/org/javacream/books/BooksConfiguration.java
@@ -0,0 +1,8 @@
+package org.javacream.books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+
+@SpringBootConfiguration
+public class BooksConfiguration {
+}
