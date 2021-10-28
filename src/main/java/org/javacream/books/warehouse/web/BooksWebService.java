1d98a6124074575412a9beaaabd1f802260dff03
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-10-28 14:31:57 +0200

    start BooksWevbService

--- src/main/java/org/javacream/books/warehouse/web/BooksWebService.java
+++ src/main/java/org/javacream/books/warehouse/web/BooksWebService.java
@@ -8,7 +8,10 @@ import org.javacream.books.warehouse.api.BooksService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,9 +23,14 @@ public class BooksWebService {
 	@Autowired private BooksService booksService;
 
 
+	@PostMapping(path = "api/books/{title}")
+	//UNPROCESSABLE_ENTITY
 	public String newBook(String title) throws BookException {
 		return booksService.newBook(title);
 	}
+
+	@GetMapping(path = "api/books/{isbn}")
+	//NOT_FOUND
 
 	public Book findBookByIsbn(String isbn) throws BookException {
 		return booksService.findBookByIsbn(isbn);
@@ -37,10 +45,14 @@ public class BooksWebService {
 		}
 	}
 
+	
+	@DeleteMapping(path = "api/books/{isbn}")
+	//Conflict
 	public void deleteBookByIsbn(String isbn) throws BookException {
 		booksService.deleteBookByIsbn(isbn);
 	}
 
+	@GetMapping(path = "api/books")
 	public Collection<Book> findAllBooks() {
 		return booksService.findAllBooks();
 	}
