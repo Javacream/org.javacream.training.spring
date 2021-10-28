5cb66dc5479c2cb4654fdc96c0996940d1f5e701
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-10-28 16:11:40 +0200

    fix data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-insert into store (category, item, stock) values ('Isbn1', 'Title1', 10)
-insert into store (category, item, stock) values ('Isbn2', 'Title2', 5)
-insert into store (category, item, stock) values ('Isbn3', 'Title3', 42)
-insert into store (category, item, stock) values ('Isbn4', 'Title4', 3518)
+insert into store (category, item, stock) values ('books', 'Isbn1', 10)
+insert into store (category, item, stock) values ('books', 'Isbn2', 5)
+insert into store (category, item, stock) values ('books', 'Isbn3', 42)
+insert into store (category, item, stock) values ('books', 'Isbn4', 3518)
