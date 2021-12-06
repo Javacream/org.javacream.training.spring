b5b5e10935dc83d854c41d6d4f0dd6354d9f74ec
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-12-06 11:30:47 +0100

    play around with spring core

--- src/main/java/org/javacream/store/web/StoreWebService.java
+++ src/main/java/org/javacream/store/web/StoreWebService.java
@@ -1,4 +1,4 @@
-package org.javacream.store.api.web;
+package org.javacream.store.web;
 
 import org.javacream.store.api.StoreService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class StoreWebService {
 
-	@Autowired private StoreService storeService;
+	@Autowired 
+	private StoreService storeService;
 
 	@GetMapping("api/store/{cat}/{item}")
 	public int getStock(@PathVariable("cat")  String category, @PathVariable("item") String item) {
