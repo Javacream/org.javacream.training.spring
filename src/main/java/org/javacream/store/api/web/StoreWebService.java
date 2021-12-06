43c24348abbed493ff59d9280df5e67e421dd875
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-12-06 10:13:02 +0100

    fix

--- src/main/java/org/javacream/store/api/web/StoreWebService.java
+++ src/main/java/org/javacream/store/api/web/StoreWebService.java
@@ -1,4 +1,4 @@
-package org.javacream.store.api;
+package org.javacream.store.api.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
