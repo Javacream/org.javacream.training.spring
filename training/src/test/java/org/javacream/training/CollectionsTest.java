91d1e099b145c6208d667f6cc0985ed432f5d01a
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-01-05 13:12:21 +0100

    play with Collections

---
+++ training/src/test/java/org/javacream/training/CollectionsTest.java
@@ -0,0 +1,46 @@
+package org.javacream.training;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+public class CollectionsTest {
+    @Test public void testList(){
+        List<String> names = new LinkedList<>(); //ab Java 11: List<String> names = List.of("", "");
+        names.add("Hugo");
+        names.add("Emil");
+        names.add("Fritz");
+        names.add("Hugo");
+        Assertions.assertEquals(4, names.size());
+        Assertions.assertTrue(names.contains("Hugo"));
+        Assertions.assertFalse(names.contains("edurd"));
+        names.clear();
+        Assertions.assertEquals(0, names.size());
+    }
+    @Test public void testSet(){
+        Set<String> names = new HashSet<>();
+        names.add("Hugo");
+        names.add("Emil");
+        names.add("Fritz");
+        names.add("Hugo");
+        Assertions.assertEquals(3, names.size());
+        Assertions.assertTrue(names.contains("Hugo"));
+        Assertions.assertFalse(names.contains("edurd"));
+        names.clear();
+        Assertions.assertEquals(0, names.size());
+    }
+
+    @Test public void testMap(){
+        Map<Integer, String> postalCodesToCity = new HashMap<>();
+        postalCodesToCity.put(81373, "Muenchen");
+        postalCodesToCity.put(30111, "Berlin");
+        postalCodesToCity.put(81373, "München");
+        Assertions.assertEquals(2, postalCodesToCity.size());
+        Assertions.assertTrue(postalCodesToCity.containsKey(81373));
+        Assertions.assertTrue(postalCodesToCity.containsValue("Berlin"));
+        postalCodesToCity.clear();
+        Assertions.assertEquals(0, postalCodesToCity.size());
+
+    }
+}
