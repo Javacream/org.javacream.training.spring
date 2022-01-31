067f319ac9d55c5b3876fa0f420071375abe0ac3
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-01-31 10:51:31 +0100

    introduce web

--- books/src/main/java/org/javacream/Application.java
+++ books/src/main/java/org/javacream/Application.java
@@ -1,7 +1,15 @@
 package org.javacream;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(Application.class);
+        app.setAdditionalProfiles("prod");
+        app.run(args);
+        System.out.println("main app is running");
+    }
 }
