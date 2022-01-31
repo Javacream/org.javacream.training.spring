8633fcdaae5fb8d07951cdad8a8664fb83f203c7
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-01-31 13:37:46 +0100

    add StoreWebService

---
+++ books/src/main/java/org/javacream/store/web/StoreWebService.java
@@ -0,0 +1,20 @@
+package org.javacream.store.web;
+
+import org.javacream.store.api.StoreService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StoreWebService{
+
+    @Autowired private StoreService storeService;
+
+    @GetMapping(path = "api/store/{cat}/{item}", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String getStock(@PathVariable("cat") String category, @PathVariable("item") String itemId) {
+        return "Stock: " + storeService.getStock(category, itemId);
+    }
+
+}
