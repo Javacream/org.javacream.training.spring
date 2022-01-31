85ff95aa47092b27787eedb424ee581c8e92d9a7
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-01-31 14:26:45 +0100

    implement IsbnGeneratorWebService

--- books/src/main/java/org/javacream/books/isbngenerator/web/IsbnGeneratorWebService.java
+++ books/src/main/java/org/javacream/books/isbngenerator/web/IsbnGeneratorWebService.java
@@ -4,17 +4,26 @@ import org.javacream.books.isbngenerator.api.IsbnGeneratorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IsbnGeneratorWebService {
 
     @PostMapping(path = "api/isbn", produces = MediaType.TEXT_PLAIN_VALUE)
-    public String next() {
-        return isbnGeneratorService.next();
+    public String next(@RequestHeader("strategy") String strategy) {
+        if ("random".equals(strategy)) {
+            return randomIsbnGeneratorService.next();
+        }else if ("sequence".equals(strategy)){
+            return sequenceIsbnGeneratorService.next();
+
+        }else{
+            return "Unsupported strategy:" + strategy;
+        }
     }
 
-    @Autowired @IsbnGeneratorService.SequenceStrategy private IsbnGeneratorService isbnGeneratorService;
+    @Autowired @IsbnGeneratorService.SequenceStrategy private IsbnGeneratorService sequenceIsbnGeneratorService;
+    @Autowired @IsbnGeneratorService.RandomStrategy private IsbnGeneratorService randomIsbnGeneratorService;
 
 
 }
