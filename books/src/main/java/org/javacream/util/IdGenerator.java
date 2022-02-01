bc0e3d2f7fd46b292acde1cffbbcc9b4623f0c25
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-02-01 15:35:45 +0100

    implement SequenceIsbnGenerator using database

--- books/src/main/java/org/javacream/util/IdGenerator.java
+++ books/src/main/java/org/javacream/util/IdGenerator.java
@@ -2,12 +2,25 @@ package org.javacream.util;
 
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import java.math.BigInteger;
 
 @Service
 @Scope("singleton")
+@Transactional
 public class IdGenerator {
-    private Long counter = 0l;
+
+    @PersistenceContext private EntityManager entityManager;
     public Long nextId(){
-        return ++counter;
+        BigInteger id = (BigInteger) entityManager.createNativeQuery("select id from id").getSingleResult();
+        BigInteger newId = id.add(BigInteger.ONE);
+        Query query = entityManager.createNativeQuery("update id set id = :id");
+        query.setParameter("id", newId);
+        query.executeUpdate();
+        return newId.longValue();
     }
 }
--- books/src/main/resources/schema.sql
+++ books/src/main/resources/schema.sql
@@ -1,2 +1,3 @@
 create table store (category varchar(50), item_id varchar(50), stock integer, primary key (category, item_id))
 create table logs (message varchar(1024))
+create table id (id long)
