b8304524d1505b8e0129c59c8a33cd763873b98e
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-03-03 13:14:06 +0100

    introduce @Value

--- projects/org.javacream.training.spring/src/main/java/org/javacream/books/isbngenerator/impl/RandomIsbnGenerator.java
+++ projects/org.javacream.training.spring/src/main/java/org/javacream/books/isbngenerator/impl/RandomIsbnGenerator.java
@@ -3,13 +3,14 @@ package org.javacream.books.isbngenerator.impl;
 import java.util.Random;
 
 import org.javacream.books.isbngenerator.api.IsbnGenerator;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RandomIsbnGenerator implements IsbnGenerator {
 
-	private String prefix;
-	private String countryCode;
+	@Value("${isbngenerator.prefix}")  String prefix;
+	@Value("${isbngenerator.countryCode}")  private String countryCode;
 	public String getCountryCode() {
 		return countryCode;
 	}
--- projects/org.javacream.training.spring/src/main/java/org/javacream/store/impl/SimpleStoreService.java
+++ projects/org.javacream.training.spring/src/main/java/org/javacream/store/impl/SimpleStoreService.java
@@ -1,12 +1,14 @@
 package org.javacream.store.impl;
 
 import org.javacream.store.api.StoreService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SimpleStoreService implements StoreService {
+	@Value("${store.defaultStock}")
 	private int stock;
-	
+
 	@Override
 	public int getStock(String category, String item) {
 		return stock;
