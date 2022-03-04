c8f092720d7795e97467c6dcea60a71ae5f79c15
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-03-04 10:24:11 +0100

    play around with scope for SequenceIdGenerator

--- projects/org.javacream.training.spring/src/main/java/org/javacream/util/SequenceIdGenerator.java
+++ projects/org.javacream.training.spring/src/main/java/org/javacream/util/SequenceIdGenerator.java
@@ -1,8 +1,10 @@
 package org.javacream.util;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 @Service
+@Scope("prototype")
 public class SequenceIdGenerator {
 
 	private long counter = 0;
---
+++ projects/org.javacream.training.spring/src/test/java/org/javacream/books/warehouse/test/OrdersAndBooksTest.java
@@ -0,0 +1,21 @@
+package org.javacream.books.warehouse.test;
+
+import org.javacream.books.order.api.OrderService;
+import org.javacream.books.warehouse.api.BookException;
+import org.javacream.books.warehouse.api.BooksService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class OrdersAndBooksTest {
+	@Autowired BooksService booksService;
+	@Autowired OrderService orderService;
+	@Test public void testOrdersAndBooks() throws BookException {
+		System.out.println(orderService.order("irgendwas", 5).getOrderId());
+		System.out.println(booksService.newBook("irgendwas"));
+		System.out.println(booksService.newBook("irgendwas"));
+		System.out.println(orderService.order("irgendwas", 5).getOrderId());
+		System.out.println(booksService.newBook("irgendwas"));
+	}
+}
