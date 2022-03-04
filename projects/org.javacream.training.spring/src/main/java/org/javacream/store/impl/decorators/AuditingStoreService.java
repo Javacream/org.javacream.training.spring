6f03de6789e641f04a0ebf3bd02ce0ea9b35a680
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-03-04 13:40:53 +0100

    working solution using @Primary

--- projects/org.javacream.training.spring/src/main/java/org/javacream/store/impl/decorators/AuditingStoreService.java
+++ projects/org.javacream.training.spring/src/main/java/org/javacream/store/impl/decorators/AuditingStoreService.java
@@ -1,12 +1,17 @@
 package org.javacream.store.impl.decorators;
 
 import org.javacream.store.api.StoreService;
+import org.javacream.store.impl.SimpleStoreService;
 import org.javacream.util.AuditService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
 
+@Service
+@Primary
 public class AuditingStoreService implements StoreService{
 	@Autowired AuditService auditService;
-	private StoreService storeService;
+	@Autowired private SimpleStoreService storeService;
 
 	public int getStock(String category, String item) {
 		auditService.log("store-request", "from decorator");
