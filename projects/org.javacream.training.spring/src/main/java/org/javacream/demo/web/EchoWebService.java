ba2a155b4cbc075f2cd367802a4b41dd3af21fcc
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-03-24 10:04:14 +0100

    add parameters

--- projects/org.javacream.training.spring/src/main/java/org/javacream/demo/web/EchoWebService.java
+++ projects/org.javacream.training.spring/src/main/java/org/javacream/demo/web/EchoWebService.java
@@ -2,13 +2,33 @@ package org.javacream.demo.web;
 
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.annotation.RequestScope;
 
 @RestController
+@RequestScope
 public class EchoWebService {
 
 	@GetMapping(path = "api/ping", produces = MediaType.TEXT_PLAIN_VALUE)
 	public String ping() {
 		return "pong";
 	}
+
+	@GetMapping(path = "api/echo/{m}", produces = MediaType.TEXT_PLAIN_VALUE)
+	public String echoWithPathVariable(@PathVariable("m") String message) {
+		return message;
+	}
+
+	@GetMapping(path = "api/echo", produces = MediaType.TEXT_PLAIN_VALUE)
+	public String echoWithHttpHeader(@RequestHeader("message") String message) {
+		return message;
+	}
+	@GetMapping(path = "api/echo/query", produces = MediaType.TEXT_PLAIN_VALUE)
+	public String echoWithQuery(@RequestParam("message") String message) {
+		return message;
+	}
+	
 }
