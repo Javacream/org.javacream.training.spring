9bab1ce840e4d5cb3ce660b404761be0501ee33f
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-03-24 14:54:30 +0100

    implement AuditService using AUDIT table

--- projects/org.javacream.training.spring/src/main/java/org/javacream/store/impl/decorators/AuditingStoreService.java
+++ projects/org.javacream.training.spring/src/main/java/org/javacream/store/impl/decorators/AuditingStoreService.java
@@ -1,5 +1,7 @@
 package org.javacream.store.impl.decorators;
 
+import java.util.Date;
+
 import org.javacream.store.api.StoreService;
 import org.javacream.util.AuditService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +11,7 @@ public class AuditingStoreService implements StoreService{
 	@Autowired private StoreService storeService;
 
 	public int getStock(String category, String item) {
-		auditService.log("store-request", "from decorator");
+		auditService.log("store-request", "category=" + category + ", item=" + item + ", at " + new Date());
 		return storeService.getStock(category, item);
 	}
 
--- projects/org.javacream.training.spring/src/main/java/org/javacream/util/AuditService.java
+++ projects/org.javacream.training.spring/src/main/java/org/javacream/util/AuditService.java
@@ -1,11 +1,21 @@
 package org.javacream.util;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class AuditService {
 
+	@Autowired private EntityManager entityManager;
 	public void log(String category, String message) {
-		System.out.println("AUDITSERVICE: category " + category + " writes message " + message);
+		Query query = entityManager.createNativeQuery("insert into AUDIT (category, message) values (:category, :message)");
+		query.setParameter("category", category);
+		query.setParameter("message", message);
+		query.executeUpdate();
 	}
 }
