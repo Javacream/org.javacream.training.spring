8a236afe58f460c7885a087e8653ae6696a929d3
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-03-25 12:43:30 +0100

    add keycloak sample

---
+++ projects/org.javacream.training.spring.security/src/main/java/org/javacream/training/spring/security/SimpleWebService.java
@@ -0,0 +1,34 @@
+package org.javacream.training.spring.security;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class SimpleWebService {
+
+    @RequestMapping(value = "/anonymous", method = RequestMethod.GET)
+    public ResponseEntity<String> getAnonymous() {
+        return ResponseEntity.ok("Hello Anonymous");
+    }
+
+    @Secured("ROLE_USER")
+    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    public ResponseEntity<String> getUser() {
+        return ResponseEntity.ok("Hello User");
+    }
+
+    @Secured("ROLE_ADMIN")
+    @RequestMapping(value = "/admin", method = RequestMethod.GET)
+    public ResponseEntity<String> getAdmin() {
+        return ResponseEntity.ok("Hello Admin");
+    }
+
+    @RequestMapping(value = "/all-user", method = RequestMethod.GET)
+    public ResponseEntity<String> getAllUser() {
+        return ResponseEntity.ok("Hello All User");
+    }
+}
