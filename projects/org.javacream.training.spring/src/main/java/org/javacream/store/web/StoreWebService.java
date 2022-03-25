014e1938aafd28c9b4537a96f3465cd5213d098c
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-03-25 13:31:56 +0100

    introduce security with OAuth2/Keycloak

--- projects/org.javacream.training.spring/src/main/java/org/javacream/store/web/StoreWebService.java
+++ projects/org.javacream.training.spring/src/main/java/org/javacream/store/web/StoreWebService.java
@@ -2,6 +2,7 @@ package org.javacream.store.web;
 
 import org.javacream.store.api.StoreService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ public class StoreWebService {
 	@Autowired private StoreService storeService;
 
 	@GetMapping(path = "api/store/{category}/{item}")
+	@Secured("ROLE_USER")
 	public int getStock(@PathVariable("category") String category, @PathVariable("item")String item) {
 		return storeService.getStock(category, item);
 	}
