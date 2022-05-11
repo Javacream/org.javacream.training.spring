f3f8bf20cb53065c279bbe459ee52663c8223a41
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-05-11 11:34:13 +0200

    introduce first spring test and component

--- src/main/java/org/javacream/store/impl/SimpleStoreService.java
+++ src/main/java/org/javacream/store/impl/SimpleStoreService.java
@@ -1,8 +1,24 @@
 package org.javacream.store.impl;
 
-import org.javacream.store.api.StoreService;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
+import org.javacream.store.api.StoreService;
+import org.springframework.stereotype.Component;
+
+@Component
 public class SimpleStoreService implements StoreService {
+	
+	{
+		System.out.println("constructing " + this);
+	}
+	@PostConstruct 
+	public void init() {
+		System.out.println("initializing " + this);
+	}
+	@PreDestroy public void remove() {
+		System.out.println("destroying " + this);
+	}
 	private int stock;
 	
 	@Override
---
+++ src/test/java/org/javacream/books/warehouse/test/SimpleUnittTest.java
@@ -0,0 +1,14 @@
+package org.javacream.books.warehouse.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SimpleUnittTest {
+
+	@Test
+	public void testIt() {
+		Assertions.assertTrue(true);
+	}
+}
