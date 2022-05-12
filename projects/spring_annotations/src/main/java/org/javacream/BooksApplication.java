c09d4a9835e09be3a77b33b6c9f13017a08f7732
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-05-12 09:08:29 +0200

    introduce @Configuration part 2

--- projects/spring_annotations/src/main/java/org/javacream/BooksApplication.java
+++ projects/spring_annotations/src/main/java/org/javacream/BooksApplication.java
@@ -4,28 +4,25 @@ import org.javacream.books.isbngenerator.api.IsbnGenerator;
 import org.javacream.books.isbngenerator.api.IsbnGenerator.SequenceStrategy;
 import org.javacream.books.isbngenerator.impl.CounterIsbnGenerator;
 import org.javacream.util.SequenceGenerator;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BooksApplication {
-	@Value("${isbngenerator.prefix}") 
-	private String prefix;
-	@Value("${isbngenerator.countryCode}") 
-	private String countryCode;
 
-	@Autowired private SequenceGenerator sequenceGenerator;
-	
-	@Bean @SequenceStrategy public IsbnGenerator sequenceIsbnGenerator() {
-		CounterIsbnGenerator counterIsbnGenerator =  new CounterIsbnGenerator();
-		//counterIsbnGenerator.setPrefix("Hugo");
-		//counterIsbnGenerator.setSequenceGenerator(new SequenceGenerator()); //MIT DAS SCHLIMMSTE WAS SIE MACHEN KÖNNEN! 
+	@Bean
+	@SequenceStrategy
+	public IsbnGenerator sequenceIsbnGenerator(@Value("${isbngenerator.prefix}") String prefix,
+			@Value("${isbngenerator.countryCode}") String countryCode, SequenceGenerator sequenceGenerator) {
+		CounterIsbnGenerator counterIsbnGenerator = new CounterIsbnGenerator();
+		// counterIsbnGenerator.setPrefix("Hugo");
+		// counterIsbnGenerator.setSequenceGenerator(new SequenceGenerator()); //MIT DAS
+		// SCHLIMMSTE WAS SIE MACHEN KÖNNEN!
 		counterIsbnGenerator.setPrefix(prefix);
 		counterIsbnGenerator.setCountryCode(countryCode);
 		counterIsbnGenerator.setSequenceGenerator(sequenceGenerator);
-		
+
 		return counterIsbnGenerator;
 	}
 }
