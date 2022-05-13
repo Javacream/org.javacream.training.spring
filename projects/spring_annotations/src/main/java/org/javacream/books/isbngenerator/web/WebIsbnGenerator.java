5f71c3afe7ae24f5879ccd4ad4494d08cb7f2376
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-05-13 10:18:52 +0200

    finish WebServices, Step 4, Request Scope

--- projects/spring_annotations/src/main/java/org/javacream/books/isbngenerator/web/WebIsbnGenerator.java
+++ projects/spring_annotations/src/main/java/org/javacream/books/isbngenerator/web/WebIsbnGenerator.java
@@ -1,15 +1,19 @@
 package org.javacream.books.isbngenerator.web;
 
+import javax.annotation.PostConstruct;
+
 import org.javacream.books.isbngenerator.api.IsbnGenerator;
 import org.javacream.books.isbngenerator.api.IsbnGenerator.RandomStrategy;
 import org.javacream.books.isbngenerator.api.IsbnGenerator.SequenceStrategy;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Scope("request")
 public class WebIsbnGenerator {
 
 	@Autowired @SequenceStrategy private IsbnGenerator sequenceIsbnGenerator;
@@ -18,12 +22,16 @@ public class WebIsbnGenerator {
 	@PostMapping(path = "api/isbn", produces = MediaType.TEXT_PLAIN_VALUE)
 	public String next(@RequestParam(name = "strategy", required = false) String strategy) {
 		if ("sequence".equals(strategy)) {
-			return sequenceIsbnGenerator.next();
+			return sequenceIsbnGenerator.next() + " " + this;
 		}else if ("random".equals(strategy)) {
 			return randomIsbnGenerator.next();
 		}else {
 			return "unknown strategy: " + strategy;
 		}
 		
+	}
+	
+	@PostConstruct public void init() {
+		System.out.println("initializing " + this);
 	}
 }
