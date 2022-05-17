1474061add7faacd35ba9f7aad756f446abd7c8a
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-05-17 14:16:16 +0200

    aop: step 3 - 5

--- projects/training/src/main/java/org/javacream/util/aspects/TracingAspect.java
+++ projects/training/src/main/java/org/javacream/util/aspects/TracingAspect.java
@@ -1,11 +1,19 @@
 package org.javacream.util.aspects;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
 public class TracingAspect {
 	
-	public Object trace() throws Throwable{
+	@Around("execution(public int org.javacream.store.impl.SimpleStoreService.getStock(String, String))")
+	public Object trace(ProceedingJoinPoint pjp) throws Throwable{
 		System.out.println("entering... ");
 		try {
-			Object result = new Object();
+			Object result = pjp.proceed();
 			System.out.println("returning from ... ");
 			return result;
 		}
