5ef72848f0d0894653b1c714b9f723a54242026e
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-05-17 11:24:56 +0200

    scratch: Decorator and Autowiring

--- projects/training/src/main/java/org/javacream/store/impl/decorators/AuditingStoreService.java
+++ projects/training/src/main/java/org/javacream/store/impl/decorators/AuditingStoreService.java
@@ -1,10 +1,16 @@
 package org.javacream.store.impl.decorators;
 
 import org.javacream.store.api.StoreService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
 
+
+@Component
+@Primary
 public class AuditingStoreService implements StoreService{
 	
-	private StoreService delegate;
+	@Autowired private StoreService delegate;
 
 	public void setDelegate(StoreService delegate) {
 		this.delegate = delegate;
---
+++ projects/training/src/test/java/org/javacream/store/test/StoreTest.java
@@ -0,0 +1,18 @@
+package org.javacream.store.test;
+
+import org.javacream.store.api.StoreService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("dev")
+public class StoreTest {
+
+	@Autowired private StoreService storeService;
+	@Test public void storeServiceWorks() {
+		storeService.getStock("this", "that");
+		
+	}
+}
