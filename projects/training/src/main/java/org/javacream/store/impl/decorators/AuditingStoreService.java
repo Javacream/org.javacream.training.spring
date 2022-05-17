2326a9b32693db99f86ef22de1e7f70f050be902
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-05-17 11:18:13 +0200

    start decorators, add AuditingStoreService

---
+++ projects/training/src/main/java/org/javacream/store/impl/decorators/AuditingStoreService.java
@@ -0,0 +1,21 @@
+package org.javacream.store.impl.decorators;
+
+import org.javacream.store.api.StoreService;
+
+public class AuditingStoreService implements StoreService{
+	
+	private StoreService delegate;
+
+	public void setDelegate(StoreService delegate) {
+		this.delegate = delegate;
+	}
+
+	public int getStock(String category, String item) {
+		System.out.println("calling getStock");
+		return delegate.getStock(category, item);
+	}
+
+	
+	
+
+}
