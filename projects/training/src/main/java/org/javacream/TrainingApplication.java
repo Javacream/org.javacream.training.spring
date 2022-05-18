98999215e380c0ddf696106b2584f92bcec8c8c7
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-05-18 09:14:50 +0200

    present PropertySource

--- projects/training/src/main/java/org/javacream/TrainingApplication.java
+++ projects/training/src/main/java/org/javacream/TrainingApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
-@SpringBootApplication  //@Configuration @ContextConfiguration("org.javacream")
+@SpringBootApplication  //@Configuration @ContextConfiguration("org.javacream") @PropertySource("classpath:application.properties")
 @ImportResource("classpath:beans.xml")
 public class TrainingApplication {
 	
