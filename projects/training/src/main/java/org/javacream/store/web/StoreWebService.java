e9861ee11a66583cadabc925196d10a73fb833f9
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-05-18 11:09:03 +0200

    play around with rest

--- projects/training/src/main/java/org/javacream/store/web/StoreWebService.java
+++ projects/training/src/main/java/org/javacream/store/web/StoreWebService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,4 +19,18 @@ public class StoreWebService{ // implements StoreService{ -> FALSCH, das REST-AP
 	public String getStock(@PathVariable("cat") String category, @PathVariable("item") String item) {
 		return "Stock" + storeService.getStock(category, item);
 	}
+
+	@GetMapping(path = "api/store/header", produces=MediaType.TEXT_PLAIN_VALUE)
+	public String getStockWithHeaders(@RequestHeader("cat") String category, @RequestHeader("item") String item) {
+		return "Stock" + storeService.getStock(category, item);
+	}
+	@GetMapping(path = "api/store/query", produces=MediaType.TEXT_PLAIN_VALUE)
+	public String getStockWithQuery(@RequestParam("cat") String category, @RequestParam("item") String item) {
+		return "Stock" + storeService.getStock(category, item);
+	}
+	@GetMapping(path = "api/store", produces=MediaType.TEXT_PLAIN_VALUE)
+	public String getStockCombi(@RequestParam("cat") String category, @RequestHeader("item") String item) {
+		return "Stock" + storeService.getStock(category, item);
+	}
+
 }
