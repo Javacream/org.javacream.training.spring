9d538bb9f1b8bb26a4b9294e312124ab64c88ad3
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-05-18 11:39:53 +0200

    introduce ResponseStatusException and HttpStatus

--- projects/training/src/main/java/org/javacream/books/isbngenerator/web/IsbnGeneratorWebService.java
+++ projects/training/src/main/java/org/javacream/books/isbngenerator/web/IsbnGeneratorWebService.java
@@ -2,10 +2,12 @@ package org.javacream.books.isbngenerator.web;
 
 import org.javacream.books.isbngenerator.api.IsbnGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class IsbnGeneratorWebService {
@@ -20,7 +22,7 @@ public class IsbnGeneratorWebService {
 		}else if("seq".equals(strategy)){
 			return sequenceIsbnGenerator.next();
 		}else {
-			return "unknown strategy: " + strategy;
+			throw new  ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, "unknown strategy: " + strategy);
 		}
 	}
 }
