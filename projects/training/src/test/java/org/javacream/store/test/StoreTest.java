b9e6da0056053079613eb8d368692902f88708b8
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-05-19 08:47:56 +0200

    implement getStock using database

--- projects/training/src/test/java/org/javacream/store/test/StoreTest.java
+++ projects/training/src/test/java/org/javacream/store/test/StoreTest.java
@@ -2,6 +2,7 @@ package org.javacream.store.test;
 
 import org.javacream.store.api.StoreService;
 import org.javacream.store.api.StoreService.Audit;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,8 +13,13 @@ import org.springframework.test.context.ActiveProfiles;
 public class StoreTest {
 
 	@Autowired @Audit private StoreService storeService;
-	@Test public void storeServiceWorks() {
-		storeService.getStock("this", "that");
+
+	@Test public void getStockForCategoryThisAndItemThatRetrieves0() {
+		Assertions.assertEquals(0, storeService.getStock("this", "that"));
+		
+	}
+	@Test public void getStockForCategoryBooksAndItemISBN1Retrieves42() {
+		Assertions.assertEquals(42, storeService.getStock("books", "ISBN1"));
 		
 	}
 }
