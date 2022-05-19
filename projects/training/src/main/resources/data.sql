1d52e4781acfd0bb68c061568ae6b913417fe07f
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-05-19 08:39:46 +0200

    start with DatabaseStoreService

--- projects/training/src/main/resources/data.sql
+++ projects/training/src/main/resources/data.sql
@@ -1,1 +1,5 @@
 insert into messages (message) values('Hello')
+insert into store (category, item, stock) values('books', 'ISBN1', 42)
+insert into store (category, item, stock) values('books', 'ISBN2', 2)
+insert into store (category, item, stock) values('books', 'ISBN3', 4)
+
--- projects/training/src/main/resources/schema.sql
+++ projects/training/src/main/resources/schema.sql
@@ -1,1 +1,2 @@
 create table messages (message varchar(1024))
+create table store (category varchar(20), item varchar(20), stock integer, primary key (category, item))
