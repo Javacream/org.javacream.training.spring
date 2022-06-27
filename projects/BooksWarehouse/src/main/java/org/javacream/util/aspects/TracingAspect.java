aff96f4bb9834acf95e04c9b6f5462c25459ae3e
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-06-27 15:26:05 +0200

    add aspect logic

--- projects/BooksWarehouse/src/main/java/org/javacream/util/aspects/TracingAspect.java
+++ projects/BooksWarehouse/src/main/java/org/javacream/util/aspects/TracingAspect.java
@@ -1,5 +1,6 @@
 package org.javacream.util.aspects;
 
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
@@ -7,4 +8,14 @@ import org.springframework.stereotype.Component;
 @Aspect
 public class TracingAspect {
 
+	public Object trace(ProceedingJoinPoint pjp) {
+		//vor der aspektierten Logik:
+		System.out.println("before method invocation");
+		//Aufruf der aspektierten Logik:
+		Object result = pjp.proceed();
+		//nach der aspektierten Logik:
+		System.out.println("after method invocation");
+		return result;
+		
+	}
 }
