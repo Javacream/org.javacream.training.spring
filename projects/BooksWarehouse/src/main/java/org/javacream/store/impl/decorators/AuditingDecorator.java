93f081208a9db2bde1ffdec057a1c4724cedd1d0
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-06-27 13:40:28 +0200

    start aspects

---
+++ projects/BooksWarehouse/src/main/java/org/javacream/store/impl/decorators/AuditingDecorator.java
@@ -0,0 +1,16 @@
+package org.javacream.store.impl.decorators;
+
+import org.javacream.store.api.StoreService;
+
+public class AuditingDecorator implements StoreService{
+	
+	private StoreService storeService;
+
+	public int getStock(String category, String item) {
+		System.out.println("called getStock...");
+		return storeService.getStock(category, item);
+	}
+	
+	
+
+}
