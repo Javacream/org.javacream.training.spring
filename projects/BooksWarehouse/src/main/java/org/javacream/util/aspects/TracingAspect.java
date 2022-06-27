954e5d58468c0477c093916bc3c2077a24672a48
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-06-27 15:21:26 +0200

    introduce aspect scratch

---
+++ projects/BooksWarehouse/src/main/java/org/javacream/util/aspects/TracingAspect.java
@@ -0,0 +1,10 @@
+package org.javacream.util.aspects;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class TracingAspect {
+
+}
