f61d61642d7c93768b8fbf99d4c46c1c777534e0
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-06-29 09:01:33 +0200

    fix BookApplication

--- projects/BooksApplication/src/main/resources/data.sql
+++ projects/BooksApplication/src/main/resources/data.sql
@@ -1,2 +1,4 @@
 insert into isbns values (0)
-
+insert into store (category, item, stock) values('books', 'ISBN1', 42)
+insert into store (category, item, stock) values('books', 'ISBN2', 2)
+insert into store (category, item, stock) values('books', 'ISBN3', 4)
--- projects/BooksApplication/src/main/resources/schema.sql
+++ projects/BooksApplication/src/main/resources/schema.sql
@@ -1,1 +1,2 @@
 create table isbns (isbn integer)
+create table store (category varchar(20), item varchar(20), stock integer, primary key(category, item))
