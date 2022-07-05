fbbcb04d55c1de797189bcbf7b16a60a5fb819a2
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-07-05 12:15:33 +0200

    introduce Spring

--- src/main/java/org/javacream/store/impl/SimpleStoreService.java
+++ src/main/java/org/javacream/store/impl/SimpleStoreService.java
@@ -1,8 +1,12 @@
 package org.javacream.store.impl;
 
 import org.javacream.store.api.StoreService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+@Component
 public class SimpleStoreService implements StoreService {
+	@Value("${storeService.defaultStock}")
 	private int stock;
 	
 	@Override
