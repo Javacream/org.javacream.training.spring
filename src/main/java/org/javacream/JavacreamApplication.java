a3744f8ae6a1e5d1cf4dd3e22b42a8914d3fbfde
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-07-05 11:47:14 +0200

    introdice Spring Boot

---
+++ src/main/java/org/javacream/JavacreamApplication.java
@@ -0,0 +1,7 @@
+package org.javacream;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication //ab jetzt: Alle Subpakete werden nach Spring-relevanten Informationen untersucht
+public class JavacreamApplication {
+}
