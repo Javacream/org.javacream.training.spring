00f73c9958e973dafa261ed425031dc7b31e609f
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-07-06 14:35:03 +0200

    introduce decorator using xml

--- src/main/java/org/javacream/store/impl/decorator/Auditing.java
+++ src/main/java/org/javacream/store/impl/decorator/Auditing.java
@@ -5,6 +5,10 @@ import org.javacream.store.api.StoreService;
 public class Auditing implements StoreService {
 
     private StoreService delegate;
+
+    public void setDelegate(StoreService storeService){
+        this.delegate = storeService;
+    }
     @Override
     public int getStock(String category, String item) {
         System.out.println("calling getStock at " + System.currentTimeMillis());
---
+++ src/test/java/org/javacream/store/test/StoreServiceTest.java
@@ -0,0 +1,16 @@
+package org.javacream.store.test;
+
+import org.javacream.store.api.StoreService;
+import org.javacream.util.SpringTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@SpringTest
+public class StoreServiceTest {
+    @Autowired
+    StoreService storeService;
+    @Test public void testStoreService(){
+        Assertions.assertNotNull(storeService.getStock("this", "that"));
+    }
+}
