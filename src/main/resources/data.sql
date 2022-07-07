e0ac8cb67b0228af5a8d834872510b7a5eccb13e
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-07-07 13:31:21 +0200

    start database access

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into store (category, item, stock) values ('books', 'Isbn1', 42)
+insert into store (category, item, stock) values ('books', 'Isbn2', 2)
+insert into store (category, item, stock) values ('books', 'Isbn3', 4)
