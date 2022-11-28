780ae5136be421b2ef91c71ddfd6075832802e17
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-11-28 12:23:52 +0100

    start spring

--- src/main/java/org/javacream/books/isbngenerator/impl/CounterIsbnGenerator.java
+++ src/main/java/org/javacream/books/isbngenerator/impl/CounterIsbnGenerator.java
@@ -1,10 +1,15 @@
 package org.javacream.books.isbngenerator.impl;
 
 import org.javacream.books.isbngenerator.api.IsbnGenerator;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+@Component
 public class CounterIsbnGenerator implements IsbnGenerator {
 
+	@Value("${isbngenerator.prefix}")
 	private String prefix;
+	@Value("${isbngenerator.countryCode}")
 	private String countryCode;
 	public String getCountryCode() {
 		return countryCode;
