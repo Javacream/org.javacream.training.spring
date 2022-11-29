93aa02f6bcd311e7ab4f20de868e22ba71662b2c
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-11-29 11:15:24 +0100

    continue profiles

--- src/test/java/org/javacream/books/warehouse/test/TestApplicationConfiguration.java
+++ src/test/java/org/javacream/books/warehouse/test/TestApplicationConfiguration.java
@@ -1,4 +1,4 @@
-package org.javacream;
+package org.javacream.books.warehouse.test;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -13,11 +13,23 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("prod") 
-public class ApplicationConfiguration {
+@Profile("test")
+public class TestApplicationConfiguration {
 
-	@Bean public Map<String, Book> booksProd(){
+	@Bean  public Map<String, Book> booksTest(){
 		HashMap<String, Book> books =  new HashMap<>();
+		Book b1 = new Book();
+		b1.setIsbn("ISBN-1001");
+		b1.setTitle("Test 1");
+		b1.setPrice(19.99);
+		books.put(b1.getIsbn(), b1);
+
+		Book b2 = new Book();
+		b2.setIsbn("ISBN-1002");
+		b2.setTitle("Test 2");
+		b2.setPrice(9.99);
+		books.put(b2.getIsbn(), b2);
+
 		return books;
 	}
 
