2aa58df377eaea6fe8509be5b841f29a34b23191
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-11-29 11:50:41 +0100

    introduce XML configuration

--- src/main/java/org/javacream/JavacreamApplicationStarter.java
+++ src/main/java/org/javacream/JavacreamApplicationStarter.java
@@ -2,8 +2,10 @@ package org.javacream;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:spring-beans.xml")
 public class JavacreamApplicationStarter {
 
 	public static void main(String[] args) {
--- src/main/java/org/javacream/store/impl/SimpleStoreService.java
+++ src/main/java/org/javacream/store/impl/SimpleStoreService.java
@@ -1,12 +1,8 @@
 package org.javacream.store.impl;
 
 import org.javacream.store.api.StoreService;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
 
-@Service
 public class SimpleStoreService implements StoreService {
-	@Value("${storeservice.stock}")
 	private int stock;
 	
 	@Override
