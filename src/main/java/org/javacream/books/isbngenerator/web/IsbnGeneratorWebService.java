eb52bbc977ddd8fa7318f2c24b8b9a32536c068f
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-11-30 10:39:01 +0100

    implement REST: StoreWebService, IsbnGenratorWebService

---
+++ src/main/java/org/javacream/books/isbngenerator/web/IsbnGeneratorWebService.java
@@ -0,0 +1,18 @@
+package org.javacream.books.isbngenerator.web;
+
+import org.javacream.books.isbngenerator.api.IsbnGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IsbnGeneratorWebService {
+
+	@Autowired private IsbnGenerator isbnGenerator;
+
+	@PostMapping(path="api/isbn", produces = MediaType.TEXT_PLAIN_VALUE)
+	public String next() {
+		return isbnGenerator.next();
+	}
+}
