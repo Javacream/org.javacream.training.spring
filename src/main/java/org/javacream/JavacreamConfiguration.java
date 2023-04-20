279431fd8c554880c7dbb2568fcf19215ff6d5b7
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-04-20 09:09:34 +0200

    play with @Configuration

--- src/main/java/org/javacream/JavacreamConfiguration.java
+++ src/main/java/org/javacream/JavacreamConfiguration.java
@@ -2,6 +2,7 @@ package org.javacream;
 
 import org.javacream.store.api.StoreService;
 import org.javacream.store.impl.SimpleStoreService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,9 +11,10 @@ import javax.annotation.PostConstruct;
 @Configuration
 public class JavacreamConfiguration {
 
+    @Value("${storeservice.defaultStock}") int defaultStock;
     public @Bean StoreService storeService(){
         SimpleStoreService storeService = new SimpleStoreService();
-        storeService.setStock(4711);
+        storeService.setStock(defaultStock);
         return storeService;
     }
 
