5e5f719473ad1e6b7c769dc59f2482f5355ba812
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-04-20 15:11:37 +0200

    implement ProfilingAspect

--- src/test/java/org/javacream/util/aspects/TracingAspect.java
+++ src/test/java/org/javacream/util/aspects/TracingAspect.java
@@ -8,6 +8,7 @@ import org.javacream.books.warehouse.api.Book;
 import org.javacream.books.warehouse.api.BookException;
 import org.javacream.books.warehouse.api.BooksService;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
@@ -15,13 +16,25 @@ import java.util.Collection;
 @Component
 @Aspect
 @Profile("aop")
+@Order(10)
 public class TracingAspect{
 
     @Around("execution(* org.javacream..impl.*Service.*(..))")
     public Object trace(ProceedingJoinPoint pjp) throws Throwable {
-        MethodSignature ms = (MethodSignature) pjp.getSignature();
-        String methodName = ms.getMethod().getName();
-        System.out.println("ASPECTJ: calling " + methodName);
-        return pjp.proceed();
+        MethodSignature signature=  (MethodSignature) pjp.getSignature();
+        String methodName = signature.getMethod().getName();
+        String className = pjp.getTarget().getClass().getName();
+        String info = "method " + methodName + " of class " + className;
+        System.out.println("entering " + info);
+        try {
+            Object result = pjp.proceed();
+            System.out.println("returning from " + info);
+            return result;
+        }
+        catch(Throwable t) {
+            System.out.println("throwing from " + info);
+            throw t;
+
+        }
     }
 }
