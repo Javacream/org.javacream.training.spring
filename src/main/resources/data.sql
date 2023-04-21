58e112e8b5772ff4046861a287ba472a0185a9b4
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-04-21 13:28:12 +0200

    fix database, add import.sql for jpa data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,2 @@
 insert into messages values ('Hello')
 insert into messages values ('JPA')
--- insert into BOOKS (isbn, title, price) values ('Isbn100', 'Title 100', 9.99)
--- insert into BOOKS (isbn, title, price) values ('Isbn101', 'Title 101', 19.99)
--- insert into BOOKS (isbn, title, price) values ('Isbn102', 'Title 102', 1.99)
