37f3c4c6d6c7b2c9c8ee94e5885fa300f47f4387
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-07-31 15:55:03 +0200

    introduce object lifecycle

--- src/main/java/org/javacream/books/isbngenerator/impl/CounterIsbnGenerator.java
+++ src/main/java/org/javacream/books/isbngenerator/impl/CounterIsbnGenerator.java
@@ -1,17 +1,35 @@
 package org.javacream.books.isbngenerator.impl;
 
 import org.javacream.books.isbngenerator.api.IsbnGenerator;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 //@Service("counterIsbnGenerator")
 @Service
-@Scope("prototype")
+//@Scope("prototype")
 @IsbnGenerator.SequenceStrategy
 public class CounterIsbnGenerator implements IsbnGenerator {
 
+	@Value("${isbngenerator.prefix}")
 	private String prefix;
+	@Value("${isbngenerator.countryCode}")
 	private String countryCode;
+	//Konstruktor-Aufruf erfolgt VOR der Dependency Injection
+	{
+		System.out.println("**************** prefix in Constructor: " + prefix);
+	}
+	@PostConstruct public void initTheObject() {
+		System.out.println("**************** prefix in @PostConstruct: " + prefix);
+	}
+
+	@PreDestroy public void destroyIt(){
+		System.out.println("**************** destroying " + this);
+	}
+
 	public String getCountryCode() {
 		return countryCode;
 	}
