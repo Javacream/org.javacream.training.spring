f39f9b080b152df5dd98f2f81db2b9e6f978642c
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-07-31 14:08:47 +0200

    introduce Profiles

--- src/test/java/org/javacream/books/warehouse/test/BooksServiceTest.java
+++ src/test/java/org/javacream/books/warehouse/test/BooksServiceTest.java
@@ -7,9 +7,11 @@ import org.javacream.store.impl.SimpleStoreService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class BooksServiceTest {
 
 	@Autowired
