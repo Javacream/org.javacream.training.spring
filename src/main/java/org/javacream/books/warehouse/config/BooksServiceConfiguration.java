426353d244896824fd9ace884f5a0895e428a1bd
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-08-01 09:23:57 +0200

    play with cglib

--- src/main/java/org/javacream/books/warehouse/config/BooksServiceConfiguration.java
+++ src/main/java/org/javacream/books/warehouse/config/BooksServiceConfiguration.java
@@ -4,6 +4,7 @@ import org.javacream.books.order.api.Order;
 import org.javacream.books.warehouse.api.Book;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -11,8 +12,9 @@ import java.util.Map;
 @Configuration
 public class BooksServiceConfiguration {
 
-    @Bean
+    @Bean @Scope("singleton")
     public Map<String, Book> booksMap(){
+        System.out.println("************ creating books map using " + this);
         Map<String, Book> books = new HashMap<>();
         Book book = new Book();
         book.setIsbn("Isbn1");
@@ -23,6 +25,11 @@ public class BooksServiceConfiguration {
     }
 
     @Bean Map<Long, Order> ordersMap(){
+        System.out.println("************ creating orders map using " + this);
+        Map<String, Book> books = booksMap();
+        books = booksMap();
+        books = booksMap();
+        books = booksMap();
         return new HashMap<>();
     }
 
