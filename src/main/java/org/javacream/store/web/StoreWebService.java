b5489f4f79f74c6dea375b415ed294da7c646672
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-08-01 13:52:33 +0200

    fix swagger-ui

---
+++ src/main/java/org/javacream/store/web/StoreWebService.java
@@ -0,0 +1,18 @@
+package org.javacream.store.web;
+
+import org.javacream.store.api.StoreService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class StoreWebService {//implements StoreService{ NIE DIE FACHLICHE SCHNITTSTELLE NUTZEN
+
+//    @GetMapping(path="api/store/{c}/{i}")
+//    public String getStock(@PathVariable("c") String category, @PathVariable("i") String item) {
+     @GetMapping(path="api/store")
+        public String getStock(@RequestHeader("cat") String category, @RequestParam("id") String item) {
+        return "Stock: " + storeService.getStock(category, item);
+    }
+
+    @Autowired private StoreService storeService;
+}
