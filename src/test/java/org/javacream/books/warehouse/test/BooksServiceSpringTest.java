adc1cbacb1e8b0172b7d3e7dbd1553a9d2f610e2
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2024-11-21 11:01:17 +0100

    introduce Spring

---
+++ src/test/java/org/javacream/books/warehouse/test/BooksServiceSpringTest.java
@@ -0,0 +1,24 @@
+package org.javacream.books.warehouse.test;
+
+import org.javacream.books.isbngenerator.api.IsbnGenerator;
+import org.javacream.books.isbngenerator.impl.RandomIsbnGenerator;
+import org.javacream.books.warehouse.api.BooksService;
+import org.javacream.books.warehouse.impl.MapBooksService;
+import org.javacream.store.impl.SimpleStoreService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BooksServiceSpringTest {
+    @Autowired
+    private BooksService booksService;
+    @Autowired private IsbnGenerator isbnGenerator;
+
+    @Test
+    public void testBusinessObjects() {
+        TestActor.doTest(booksService);
+        System.out.println(isbnGenerator.next());
+
+    }
+}
