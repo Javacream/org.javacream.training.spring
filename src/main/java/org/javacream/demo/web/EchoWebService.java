d3cff58159c9c63ca3e2fb3ec3e4a36b960b1570
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2024-11-22 10:27:27 +0100

    Spring Kafka

--- src/main/java/org/javacream/demo/web/EchoWebService.java
+++ src/main/java/org/javacream/demo/web/EchoWebService.java
@@ -1,13 +1,16 @@
 package org.javacream.demo.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class EchoWebService {
-
+    @Autowired private KafkaTemplate kafkaTemplate;
     @GetMapping(path = "api/demo/ping", produces = MediaType.TEXT_PLAIN_VALUE)
     public String ping(){
+        kafkaTemplate.send("topic1", "Hello Spring!");
         return "pong";
     }
     //http://localhost:9090/api/demo/echo/Hugo
