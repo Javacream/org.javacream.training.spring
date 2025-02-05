598ae927be1001494a080110aa8b0655673e8a17
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-02-05 11:36:41 +0100

    SpringBootApplication und SpringBootTest

---
+++ src/main/java/org/javacream/BooksApplication.java
@@ -0,0 +1,11 @@
+package org.javacream;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksApplication {
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(BooksApplication.class);
+        app.run(args);
+    }
+}
