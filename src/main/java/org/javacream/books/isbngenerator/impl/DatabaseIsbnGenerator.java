b3ad5e0a7f7f41477d385645a1062c880d896f04
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-02-06 13:39:31 +0100

    start programming database access

--- src/main/java/org/javacream/books/isbngenerator/impl/DatabaseIsbnGenerator.java
+++ src/main/java/org/javacream/books/isbngenerator/impl/DatabaseIsbnGenerator.java
@@ -1,17 +1,29 @@
 package org.javacream.books.isbngenerator.impl;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.javacream.books.isbngenerator.api.IsbnGenerator;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.sql.DataSource;
 
 @Service
 @IsbnGenerator.SequenceStrategy
 @Profile("prod")
 public class DatabaseIsbnGenerator implements IsbnGenerator {
 
+	@PersistenceContext
+	private EntityManager em;
+
+	@Transactional(propagation = Propagation.REQUIRED)
 	public String next(){
-		return "TO BE IMPLEMENTED";
+		int isbn = em.createNativeQuery("select * from ISBNS").getFirstResult();
+		//TODO Korrektes Auslesen sowie Hochzählen und zurückschreiben der ISBN
+		return "DATA-BASE: " + isbn;
 	}
 
 }
