e8dfd54da569fa7c4660ec917f3f1ec88f587710
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-03-31 15:48:45 +0200

    Java Config, Part 2, StoreService via JavaConfig

--- src/main/java/org/javacream/BooksApplication.java
+++ src/main/java/org/javacream/BooksApplication.java
@@ -1,7 +1,18 @@
 package org.javacream;
 
+import org.javacream.store.api.StoreService;
+import org.javacream.store.impl.SimpleStoreService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BooksApplication {
+
+    @Bean
+    public StoreService storeService(@Value("${store.defaultStock}") int defaultStock){
+        SimpleStoreService simpleStoreService = new SimpleStoreService();
+        simpleStoreService.setStock(defaultStock);
+        return simpleStoreService;
+    }
 }
--- src/main/java/org/javacream/store/impl/SimpleStoreService.java
+++ src/main/java/org/javacream/store/impl/SimpleStoreService.java
@@ -4,9 +4,13 @@ import org.javacream.store.api.StoreService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-@Service
+//@Service
 public class SimpleStoreService implements StoreService {
-	@Value("${store.defaultStock}") private int stock;
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+
+	private int stock;
 	
 	@Override
 	public int getStock(String category, String item) {
