fa5491596044560dd90cf2d3ec3112a6fea6d770
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-03-31 11:55:05 +0200

    Lifecycle

--- src/main/java/org/javacream/books/isbngenerator/impl/RandomIsbnGenerator.java
+++ src/main/java/org/javacream/books/isbngenerator/impl/RandomIsbnGenerator.java
@@ -2,6 +2,8 @@ package org.javacream.books.isbngenerator.impl;
 
 import java.util.Random;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.javacream.books.isbngenerator.api.IsbnGenerator;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -20,6 +22,15 @@ public class RandomIsbnGenerator implements IsbnGenerator {
 	public RandomIsbnGenerator(@Value("${isbngenerator.prefix}") String prefix, @Value("${isbngenerator.countryCode}") String countryCode) {
 		this.prefix = prefix;
 		this.countryCode = countryCode;
+	}
+
+	@PostConstruct
+	public void initRig(){
+		System.out.println("initializing " + this);
+	}
+	@PreDestroy
+	public void finalizeRig(){
+		System.out.println("initializing " + this);
 	}
 
 	public String next(){
