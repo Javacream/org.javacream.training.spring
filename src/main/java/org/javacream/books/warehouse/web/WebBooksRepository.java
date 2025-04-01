defa428006be2c9c2c35a66705ab52ee811388f4
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-04-01 16:01:16 +0200

    database access with spring data repository

---
+++ src/main/java/org/javacream/books/warehouse/web/WebBooksRepository.java
@@ -0,0 +1,19 @@
+package org.javacream.books.warehouse.web;
+
+import org.javacream.books.warehouse.api.Book;
+import org.javacream.books.warehouse.api.BooksRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+public class WebBooksRepository {
+    @Autowired
+    BooksRepository booksRepository;
+
+    @GetMapping(path = "api/booksrepo", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Book> findByIsbn(){
+        return booksRepository.findAll();
+}
