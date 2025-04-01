5f5aa23cc06a137ac90755c39475152973543678
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-04-01 14:12:20 +0200

    fix StoreWebService

--- src/main/java/org/javacream/store/web/StoreWebService.java
+++ src/main/java/org/javacream/store/web/StoreWebService.java
@@ -2,6 +2,7 @@ package org.javacream.store.web;
 
 import org.javacream.store.api.StoreService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class StoreWebService {
 
-    @Autowired private StoreService storeService;
+    @Autowired @Qualifier("books") private StoreService storeService;
 
     @GetMapping(path = "api/store/{cat}")
     public String getStock(@PathVariable("cat") String category, @RequestHeader("item") String item) {
