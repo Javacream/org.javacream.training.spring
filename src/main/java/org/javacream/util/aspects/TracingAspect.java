05d3886d4c6fb6c3068d4503472cfe6c8078b4f1
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-04-01 11:01:31 +0200

    Aspect oriented programming, finish impl

--- src/main/java/org/javacream/util/aspects/TracingAspect.java
+++ src/main/java/org/javacream/util/aspects/TracingAspect.java
@@ -3,17 +3,23 @@ package org.javacream.util.aspects;
 
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
 public class TracingAspect {
     public Object trace(ProceedingJoinPoint pjp) throws Throwable{
+        MethodSignature signature = (MethodSignature) pjp.getSignature();
+        String methodName = signature.getMethod().getName();
+        System.out.println("entering " + methodName);
         try {
             Object result = pjp.proceed(); // Aufruf das Delegationsobjekt
+            System.out.println("returning from " + methodName + ", result=" + result );
             return result;
         }
         catch(Throwable t){
+            System.out.println("throwing from " + methodName + ", throwable=" + t );
             throw t;
         }
     }
