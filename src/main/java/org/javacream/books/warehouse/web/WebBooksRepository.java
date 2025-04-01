74c59a962337ddc2f3bb679650cdf1ecba0f2f60
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-04-01 16:02:32 +0200

    database access with spring data repository, fix

--- src/main/java/org/javacream/books/warehouse/web/WebBooksRepository.java
+++ src/main/java/org/javacream/books/warehouse/web/WebBooksRepository.java
@@ -6,14 +6,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@RestController
 public class WebBooksRepository {
     @Autowired
     BooksRepository booksRepository;
 
     @GetMapping(path = "api/booksrepo", produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Book> findByIsbn(){
+    public List<Book> findByIsbn() {
         return booksRepository.findAll();
+    }
 }
