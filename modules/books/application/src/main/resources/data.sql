cf423a0c7dcd3c33e12f9b9ac19730f6b2341bfc
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-04-07 14:12:28 +0200

    cleanup Inter Service Communication

--- modules/books/application/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO STORE (category, item, stock) VALUES ('books', 'ISBN1', 42)
-INSERT INTO STORE (category, item, stock) VALUES ('books', 'ISBN2', 2)
-INSERT INTO STORE (category, item, stock) VALUES ('books', 'ISBN3', 4)
